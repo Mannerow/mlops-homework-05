a7fd80db55a5abaa29e2079340e548ced485de79
Mannerow <mannerow145@gmail.com>
2024-06-24 23:07:24 +0000

    Monitoring with Evidently + Grafana

--- evidently_metrics.py
+++ evidently_metrics.py
@@ -13,7 +13,7 @@ from prefect import task, flow
 
 from evidently.report import Report
 from evidently import ColumnMapping
-from evidently.metrics import ColumnDriftMetric, DatasetDriftMetric, DatasetMissingValuesMetric
+from evidently.metrics import ColumnDriftMetric, DatasetDriftMetric, DatasetMissingValuesMetric, RegressionQualityMetric, ColumnQuantileMetric
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s [%(levelname)s]: %(message)s")
 
@@ -26,7 +26,9 @@ create table dummy_metrics(
 	timestamp timestamp,
 	prediction_drift float,
 	num_drifted_columns integer,
-	share_missing_values float
+	share_missing_values float,
+	median_fare_amount float,
+	rmse float
 )
 """
 
@@ -36,20 +38,25 @@ with open('models/lin_reg.bin', 'rb') as f_in:
 
 raw_data = pd.read_parquet('data/green_tripdata_2024-03.parquet')
 
-begin = datetime.datetime(2022, 2, 1, 0, 0)
+#Need to read the data day by day
+begin = datetime.datetime(2024, 3, 1, 0, 0)
 num_features = ['passenger_count', 'trip_distance', 'fare_amount', 'total_amount']
 cat_features = ['PULocationID', 'DOLocationID']
+
+target = "duration_min"
 column_mapping = ColumnMapping(
+    target=target,
     prediction='prediction',
     numerical_features=num_features,
-    categorical_features=cat_features,
-    target=None
+    categorical_features=cat_features
 )
 
-report = Report(metrics = [
+report = Report(metrics=[
     ColumnDriftMetric(column_name='prediction'),
     DatasetDriftMetric(),
-    DatasetMissingValuesMetric()
+    DatasetMissingValuesMetric(),
+    RegressionQualityMetric(),
+    ColumnQuantileMetric(column_name="fare_amount", quantile=0.5)
 ])
 
 @task
@@ -63,32 +70,39 @@ def prep_db():
 
 @task
 def calculate_metrics_postgresql(curr, i):
-	current_data = raw_data[(raw_data.lpep_pickup_datetime >= (begin + datetime.timedelta(i))) &
-		(raw_data.lpep_pickup_datetime < (begin + datetime.timedelta(i + 1)))]
+    #filter datetime
+    current_data = raw_data[(raw_data.lpep_pickup_datetime >= (begin + datetime.timedelta(i))) &
+        (raw_data.lpep_pickup_datetime < (begin + datetime.timedelta(i + 1)))]
+	
+    current_data["duration_min"] = current_data.lpep_dropoff_datetime - current_data.lpep_pickup_datetime
+    current_data.duration_min = current_data.duration_min.apply(lambda td : float(td.total_seconds())/60)
 
-	#current_data.fillna(0, inplace=True)
-	current_data['prediction'] = model.predict(current_data[num_features + cat_features].fillna(0))
+    #current_data.fillna(0, inplace=True)
+    current_data['prediction'] = model.predict(current_data[num_features + cat_features].fillna(0))
 
-	report.run(reference_data = reference_data, current_data = current_data,
-		column_mapping=column_mapping)
+    report.run(reference_data = reference_data, current_data = current_data,
+        column_mapping=column_mapping)
 
-	result = report.as_dict()
+    result = report.as_dict()
 
-	prediction_drift = result['metrics'][0]['result']['drift_score']
-	num_drifted_columns = result['metrics'][1]['result']['number_of_drifted_columns']
-	share_missing_values = result['metrics'][2]['result']['current']['share_of_missing_values']
+    prediction_drift = result['metrics'][0]['result']['drift_score']
+    num_drifted_columns = result['metrics'][1]['result']['number_of_drifted_columns']
+    share_missing_values = result['metrics'][2]['result']['current']['share_of_missing_values']
+    median_fare_amt = result['metrics'][4]['result']['current']['value']
+    rmse = result['metrics'][3]['result']['current']['rmse']
 
-	curr.execute(
-		"insert into dummy_metrics(timestamp, prediction_drift, num_drifted_columns, share_missing_values) values (%s, %s, %s, %s)",
-		(begin + datetime.timedelta(i), prediction_drift, num_drifted_columns, share_missing_values)
-	)
+    curr.execute(
+        "insert into dummy_metrics(timestamp, prediction_drift, num_drifted_columns, share_missing_values, median_fare_amount, rmse) values (%s, %s, %s, %s, %s, %s)",
+        (begin + datetime.timedelta(i), prediction_drift, num_drifted_columns, share_missing_values, median_fare_amt, rmse)
+    )
 
 @flow
 def batch_monitoring_backfill():
 	prep_db()
 	last_send = datetime.datetime.now() - datetime.timedelta(seconds=10)
 	with psycopg.connect("host=localhost port=5432 dbname=test user=postgres password=example", autocommit=True) as conn:
-		for i in range(0, 27):
+		#31 days in march
+		for i in range(0, 30):
 			with conn.cursor() as curr:
 				calculate_metrics_postgresql(curr, i)
 
